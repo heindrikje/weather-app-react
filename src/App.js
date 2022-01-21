e523ad38b39409011f940df0f019d4f367f62585
heindrikje <heindrikje.kuhs@gmail.com>
2022-01-21 02:51:14 +0100

    Added container

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import SearchForm from "./SearchForm";
 export default function App() {
   return (
     <div className="App">
-      <div className="card">
-        <SearchForm defaultCity="Paris" />
+      <div className="container">
+        <div className="card">
+          <SearchForm defaultCity="Paris" />
+        </div>
+        <footer>
+          <a
+            href="https://github.com/heindrikje/weather-app-react"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link"
+          >
+            Open-source code
+          </a>{" "}
+          by Heindrikje Kuhs
+        </footer>
       </div>
-      <footer>
-        <a
-          href="https://github.com/heindrikje/weather-app-react"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="link"
-        >
-          Open-source code
-        </a>{" "}
-        by Heindrikje Kuhs
-      </footer>
     </div>
   );
 }
